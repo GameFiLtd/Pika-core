91beb4ca4c626b39ba99dd33b9f2ffccec2eaf1d
Daniel Gretzke <daniel@gretzke.de>
2021-08-10 23:52:06 +0200

    moved uniswap router interface to separate file

--- contracts/Pika.sol
+++ contracts/Pika.sol
@@ -3,36 +3,7 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "./OwnedInitializable.sol";
-
-interface IUniswapV2Router {
-    function factory() external pure returns (address);
-
-    function WETH() external pure returns (address);
-
-    function addLiquidityETH(
-        address token,
-        uint256 amountTokenDesired,
-        uint256 amountTokenMin,
-        uint256 amountETHMin,
-        address to,
-        uint256 deadline
-    )
-        external
-        payable
-        returns (
-            uint256 amountToken,
-            uint256 amountETH,
-            uint256 liquidity
-        );
-
-    function swapExactTokensForETHSupportingFeeOnTransferTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external;
-}
+import "./interfaces/IUniswapV2Router.sol";
 
 contract Pika is OwnedInitializable, ERC20PermitUpgradeable {
     address public WETH;
