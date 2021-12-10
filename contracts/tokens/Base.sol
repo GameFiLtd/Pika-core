b7c6f3e903df10cc772cd9cb1f1292ec984c1a5c
gretzke <daniel@gretzke.de>
2021-12-10 10:11:09 +0100

    Solidity version bump to 0.8.10

--- contracts/tokens/Base.sol
+++ contracts/tokens/Base.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.4;
+pragma solidity 0.8.10;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "../OwnedInitializable.sol";
